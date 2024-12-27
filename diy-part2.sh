f78af4c6b69623f4eab6d0afd8596fca8d5920ee
Kyxie <kyriexie@outlook.com>
2024-12-26 19:01:11 -0500

    update diy-part2

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,23 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.66/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.250/g' package/base-files/files/etc/config/network
 
-# Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# # Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/KyxieWrt/g' package/base-files/files/bin/config_generate
+
+# Modify opkg sources to custom URLs
+cat > package/feeds/packages/opkg.conf <<EOF
+src/gz openwrt_core https://downloads.openwrt.org/releases/23.05.0/targets/x86/64/packages
+src/gz openwrt_base https://downloads.openwrt.org/releases/23.05.0/packages/x86_64/base
+src/gz openwrt_luci https://downloads.openwrt.org/releases/23.05.0/packages/x86_64/luci
+src/gz openwrt_packages https://downloads.openwrt.org/releases/23.05.0/packages/x86_64/packages
+src/gz openwrt_routing https://downloads.openwrt.org/releases/23.05.0/packages/x86_64/routing
+src/gz openwrt_telephony https://downloads.openwrt.org/releases/23.05.0/packages/x86_64/telephony
+EOF
+
+# Set timezone to Toronto (Eastern Time)
+sed -i 's/option timezone.*/option timezone   "EST5EDT,M3.2.0,M11.1.0"/' package/base-files/files/etc/config/system
